f31ef41160d2c0252ee8f5372f62aa9ed4fdbda5
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-03-03 21:29:13 +0100

    fix collectionName

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var mongoHostPort = process.env.MONGODB_PORT ? process.env.MONGODB_PORT.replace(
 
 module.exports = {
   "connectionURI": 'mongodb://' + mongoHostPort + '/roadmapjs',
+  "collectionName": "data",
   "browserifyModules" : [ "qs", "async", "jquery",  "react", "react-dom" ],
   "rootURL" : "index.html",
   "routes": [
