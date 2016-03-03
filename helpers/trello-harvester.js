d75589e049c971aeb8d0386ae1636aba5e026db3
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-03-03 11:17:03 +0100

    refactoring : harvester -> helpers

--- helpers/trello-harvester.js
+++ helpers/trello-harvester.js
@@ -1,29 +1,18 @@
-#!/usr/bin/env node
-
-/**
- * Ce script se charge de moissoner les board trello 
- * un par un pour récupérer les cartes puis de précalculer
- * les dates et les plages de dates en prévision d'être
- * affiché coté client dans l'application reactjs
- *
- * Le script peut être appelé en mode ligne de commande
- * (c'est pour cela qu'il est exécutable)
- * ou bien via nodejs et require (module nodejs) 
- */
-
 'use strict';
 
-var Trello   = require('node-trello');
-var moment   = require('moment');
-var fs       = require('fs');
-var marked   = require('marked');
-var async    = require('async');
-var debug    = require('debug')('trello');
-var config   = require('./config.js');
+var path = require('path')
+  , basename = path.basename(__filename, '.js')
+  , debug = require('debug')('castor:helpers:' + basename)
+  , Trello   = require('node-trello')
+  , moment   = require('moment')
+  , fs       = require('fs')
+  , marked   = require('marked')
+  , async    = require('async')
+  ;
 
 var errors = [];
 
-module.exports = function (trelloHarvesterCb) {
+module.exports = function (targetDirectory,  config, trelloHarvesterCb) {
   moment.locale('fr');
   var t = new Trello(config.key, config.token);
 
@@ -52,19 +41,19 @@ module.exports = function (trelloHarvesterCb) {
                 // then range[0] = 2014 and range[1] = 2015
                 if (new RegExp('^[0-9]+$').test(range[0])) {
                   range[1] = '' + (parseInt(range[0], 10) + 1);
-                }                
+                }
               }
               // fix date string (ex: Aout vs Août)
               range = range.map(function (d) {
                 return d.replace('Aout', 'Août')
-                        .replace('Fevrier', 'Février')
-                        .replace('Decembre', 'Décembre');
+                .replace('Fevrier', 'Février')
+                .replace('Decembre', 'Décembre');
               });
               // parse date string as javascript date
               range = range.map(function (d) {
                 if (new RegExp('^[0-9]+$').test(d)) {
                   // if trello list name is for example 2014
-                  return moment(d, "YYYY");  
+                  return moment(d, "YYYY");
                 } else {
                   // if trello list name is for example Décembre 2014
                   return moment(d, "MMMM YYYY");
@@ -93,7 +82,9 @@ module.exports = function (trelloHarvesterCb) {
         // get the board cards one by one
         t.get("/1/boards/" + board.idBoard + "/cards", { attachments: true },
           function (err, data) {
-            if (err) return cb(err);
+            if (err) {
+              return cb(err);
+            }
 
             // browse the board cards by cards
             debug('[' + boardKey + '] The "' + boardInfo.name + '" board contains ' + data.length + ' cards');
@@ -116,53 +107,52 @@ module.exports = function (trelloHarvesterCb) {
               result.desc   = marked(item.desc);
               result.trelloLink = item.url;
               result.image_small  =
-                item.attachments.length > 0 && item.attachments[0].previews[0] ?
-                item.attachments[0].previews[0].url : '';
+              item.attachments.length > 0 && item.attachments[0].previews[0] ?
+              item.attachments[0].previews[0].url : '';
               result.image_medium =
-                item.attachments.length > 0 && item.attachments[0].previews[2] ?
-                item.attachments[0].previews[2].url : '';
+              item.attachments.length > 0 && item.attachments[0].previews[2] ?
+              item.attachments[0].previews[2].url : '';
               result.image_big =
-                item.attachments.length > 0 && item.attachments[0].previews[4] ?
-                item.attachments[0].previews[4].url : '';
+              item.attachments.length > 0 && item.attachments[0].previews[4] ?
+              item.attachments[0].previews[4].url : '';
               debug('[' + boardKey + '] This card has been found: ' + item.name);
               result.labels = [];
               item.labels.forEach(function (elt) {
-                result.labels.push(elt.name); 
+                result.labels.push(elt.name);
               });
               project.push(result);
             });
 
             return cb(null, boardInfo, project);
           });
+        }
+      ], function (err, boardInfo, project) {
+        if (err) {
+          return cbBoardFinished(err);
+        }
+
+        // write project JSON data
+        debug('[' + boardKey + '] Project ' + boardKey + ' written (with ' + project.length + ' cards)');
+        fs.writeFileSync(
+          path.join(targetDirectory, boardKey + '.json'),
+          JSON.stringify(project, null, '  ')
+        );
+
+        debug('[' + boardKey + '][finished] Parsing trello board ' + boardKey);
+        console.error(boardKey + ' OK.'); // show a dot to tell the user a board has been handled
+        cbBoardFinished(null); // on a terminé de traiter ce tableau
+      });
+
+    }, function (err) {
+      // en cas d'erreur dans le traitement de la liste des tableaux
+      if (err) {
+        var errMsg = 'Error:' + err;
+        errors.push([err, errMsg ]);
+        console.error(errMsg);
       }
-    ], function (err, boardInfo, project) {
-      if (err) return cbBoardFinished(err);
-
-      // write project JSON data
-      debug('[' + boardKey + '] Project ' + boardKey + ' written (with ' + project.length + ' cards)');
-      fs.writeFileSync(
-        __dirname + '/data/' + boardKey + '.json',
-        JSON.stringify(project, null, '  ')
-      );
-
-      debug('[' + boardKey + '][finished] Parsing trello board ' + boardKey);
-      console.error(boardKey + ' OK.'); // show a dot to tell the user a board has been handled
-      cbBoardFinished(null); // on a terminé de traiter ce tableau
+      debug('Every boards are parsed.');
+      if (typeof trelloHarvesterCb === 'function') {
+        trelloHarvesterCb(err);
+      }
     });
-
-  }, function (err) {
-    // en cas d'erreur dans le traitement de la liste des tableaux
-    if (err) {
-      var errMsg = 'Error:' + err;
-      errors.push([err, errMsg ]);
-      console.error(errMsg);
-    }
-    debug('Every boards are parsed.');
-    trelloHarvesterCb && trelloHarvesterCb(err);
-  });
-}
-
-// run the file if called as a command line
-if (!module.parent) {
-  module.exports();  
-}
+  }
